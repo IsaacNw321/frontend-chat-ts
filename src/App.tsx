669854dae15870b1330d98c72373f46e4d3b6dbf
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-10-29 11:44:20 -0400

    add: Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/Landing'
+import Navigation from './components/Nav'
 function App() {
 
   return (
     <BrowserRouter>
+      <Navigation/>
       <Routes>
         <Route path='/' element={<LandingPage/>} />
       </Routes>
