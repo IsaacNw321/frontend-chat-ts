2cce74ef12382d04645a6fc908b9a319849dd39c
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-11-03 11:49:23 -0400

    add: hook useApi

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,35 +1,38 @@
 import axios from "axios";
 import type { User } from "../types";
-
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3000', 
+  withCredentials: true, 
+});
 export const getUsers = async () =>{
-  const users =  await axios.get("http://localhost:3000/users") 
+  const users =  await apiClient.get("/users") 
   return users.status === 200 ? 
   users.data : 
   users.statusText
 }
 
 export const getUserById = async (id: string) =>{
-  const  user = await axios.get(`http://localhost:3000/users/${id}`) 
+  const  user = await apiClient.get(`/users/${id}`) 
   return user.status === 200 ? 
     user.data : 
     user.statusText;
 }
 
 export const createUser = async (User : User) =>{
-  const newUser = await axios.post("http://localhost:3000/auth/signup", User)
+  const newUser = await apiClient.post("/auth/signup", User)
   return newUser.status === 201 ? 
   newUser.headers  :
   newUser.statusText
 }
 
 export const deleteUser = async (id :string) =>{
-  const deletedUser = await axios.delete(`http://localhost:3000/users/${id}`) 
+  const deletedUser = await apiClient.delete(`/users/${id}`) 
   return deletedUser.status === 200 ?
     deletedUser.status : 
     deletedUser.statusText
 }
 
 export const pathcUser = async (id :string, data : User) =>{
-  const updateddUser = await axios.patch(`http://localhost:3000/users/${id}`,data) 
+  const updateddUser = await apiClient.patch(`/users/${id}`,data) 
   return updateddUser
 }
