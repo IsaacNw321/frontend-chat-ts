6c09d389e8b0767f9047ee86e4176ccbce92110a
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-11-23 13:03:23 -0400

    add: role to authContext for userList filter

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,5 +1,5 @@
 import { useApiQuery } from "../hooks/useApi";
-import type { User } from "../types";
+import { Role, type User } from "../types";
 import { ButtonsCard } from "./ButtonsCard";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
@@ -7,40 +7,51 @@ import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import useAuth from "../hooks/useAuth";
 import { useFilteredUsers } from "../hooks/useFilters";
 import { Filters } from "./FiltersSelect";
+
 export const UserList = () => {
-    const {id} = useAuth()
+    const {id, role} = useAuth()
     const navigate = useNavigate();
     const controller = new AbortController()
-      const axiosPrivate = useAxiosPrivate();
+    const axiosPrivate = useAxiosPrivate();
+    
     const getUsers = async() =>{
-      const users = await axiosPrivate.get('/users',{
-        signal : controller.signal
-      })
-      return  users.data
+        const users = await axiosPrivate.get('/users',{
+            signal : controller.signal
+        })
+        return users.data
     }
     
     const { data: users, isLoading, isError } = useApiQuery('users', getUsers); 
+    
     useEffect(() => {
         if (isError) {
             navigate('/');
         }
     }, [isError, navigate]);
-    const filteredUsers = useFilteredUsers(users)
+    
+    const dataForFilterHook = users ?? []; 
+
+    const roleFilteredData = role === Role.USER
+        ? dataForFilterHook.filter((user : User) => user.role === Role.SUPERUSER )
+        : dataForFilterHook;
+    
+    const filteredUsers = useFilteredUsers(roleFilteredData);
+
     if (isLoading) {
         return <div className="user-list-status">Cargando usuarios...</div>;
     }
     
     if (isError) {
         return <div className="user-list-status user-list-error">Ha habido un error al cargar los usuarios.</div>;
-        
     }
 
-    if (!users || users.length === 0) {
+    if (!filteredUsers || filteredUsers.length === 0) {
         return <div className="user-list-status">No hay usuarios disponibles.</div>;
     }
+    
     return (
         <section className="user-list-container">
-           <Filters/>
+            {role === Role.SUPERUSER ? <Filters/> : null}
             <ul className="user-cards-grid">
                 {filteredUsers.filter((user : User) => user.id !== id).map((user: User) => (
                     <li key={user.id} className="user-card">
--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,44 +1,54 @@
 import { useState, createContext, type Dispatch, type SetStateAction } from "react";
 import { useNavigate } from "react-router-dom";
 import { apiClient } from "../utils/users";
+import { Role } from "../types";
+
 export interface AuthState {
-    access_token?: string ;
+  access_token?: string ;
 }
+
 export interface AuthContextType {
+  auth: AuthState;
+  setAuth: Dispatch<SetStateAction<AuthState>>;
+  handleLogout: () => Promise<void>; 
+}
+
+export interface UseAuthReturnType {
     auth: AuthState;
     setAuth: Dispatch<SetStateAction<AuthState>>;
-    handleLogout: () => Promise<void>; 
-    id?: string | null;
+    handleLogout: () => Promise<void>;
+    id: string | null;
+    role: Role | null; 
 }
 
-export const AuthContext = createContext<AuthContextType | null>(null);
+export const AuthContext = createContext<Omit<AuthContextType, 'id' | 'role'> | null>(null);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const [auth, setAuth] = useState<AuthState>({ access_token: '' });
-    const navigate = useNavigate();
-    
-    const handleLogout = async () => {
-        try {
-            const response = await apiClient.post('/auth/logout'); 
-            console.log(response)
-            setAuth({}); 
-            navigate('/');
-        } catch (err) {
-            console.error("Logout API failed (Cookie might not have cleared):", err);
-            setAuth({}); 
-            navigate('/');
-        }
-    };
+  const [auth, setAuth] = useState<AuthState>({ access_token: '' });
+  const navigate = useNavigate();
+  
+  const handleLogout = async () => {
+   try {
+      await apiClient.post('/auth/logout'); 
+      setAuth({}); 
+      navigate('/');
+    } catch (err) {
+      console.error("Logout API failed (Cookie might not have cleared):", err);
+      setAuth({}); 
+      navigate('/');
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={{
-            auth,
-            setAuth,
-            handleLogout
-        } as AuthContextType}>
-            {children}
-        </AuthContext.Provider>
-    );
+
+  return (
+    <AuthContext.Provider value={{
+      auth,
+      setAuth,
+      handleLogout
+    }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
