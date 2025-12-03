892e6d93fd5c08d977445bbc6272d71f0de8df4b
Usuario <135558153+IsaacNw321@users.noreply.github.com>
2025-12-03 00:53:37 -0400

    refactor: Chat componet in request useMuation for createChat

--- src/utils/chats.ts
+++ src/utils/chats.ts
@@ -1,36 +1,47 @@
 import type { ChatWithMessages, ChatWitUsers, PostChatPayload } from "../types/Chats";
 import axios from "axios";
+import {type AxiosInstance, isAxiosError } from "axios";
 import type { Message, postMessage } from "../types/Messages";
 import { apiClient } from "./users"; 
 
-export const getChatByUsers = async (userIds: string[]): Promise<ChatWitUsers | undefined> => {
+export const getChatByUsers = async (
+  axiosPrivate: AxiosInstance,
+  userIds: string[], 
+  signal: AbortSignal
+): Promise<ChatWitUsers | undefined> => {
   try {
-    const response = await apiClient.get<ChatWitUsers>(`/chats/find`, {
+    const response = await axiosPrivate.get<ChatWitUsers>(`/chats/find`, {
       params: {
-     userIds: userIds
+        userIds: userIds
       },
+      signal: signal
     });
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error) && error.response?.status === 404) {
-      return undefined;
+    if (isAxiosError(error) && error.response?.status === 404) {
+      return undefined; 
+    }
+    if (isAxiosError(error) && error.code !== 'ERR_CANCELED') {
+      throw error;
     }
     throw error;
   }
-}
+};
 
-export const createChat = async (payload : PostChatPayload): Promise<ChatWitUsers> =>{
- try {
-  const response = await apiClient.post<ChatWitUsers>(`/chats`, payload);
-  return response.data;
- } catch (error) {
-
-    if (axios.isAxiosError(error)) {
-        throw new Error(error.response?.data?.message || error.response?.statusText || "Failed to create chat");
+export const createChat = async (
+  axiosPrivate: AxiosInstance,
+  payload: PostChatPayload
+): Promise<ChatWitUsers> => {
+  try {
+    const response = await axiosPrivate.post<ChatWitUsers>(`/chats`, payload);
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || error.response?.statusText || "Failed to create chat");
     }
-  throw error;
- }
-}
+    throw error;
+  }
+};
 
 
 export const getChatById = async (chatId: string): Promise<ChatWithMessages> => {
